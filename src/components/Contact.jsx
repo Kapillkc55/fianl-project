f982e91525b41a4c886dc611afeb64f4195801dc
Kapillkc55 <152358438+Kapillkc55@users.noreply.github.com>
2025-07-27 19:12:46 +0545

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -140,6 +140,7 @@ const Contact = () => {
               ) : (
                 <>
                   Send Message
+                  {/* Updated icon: Paper airplane */}
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                   </svg>
